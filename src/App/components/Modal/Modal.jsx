e983d83a4a211dbc73f9a521790df7950741aedb
Igor Kolesnik <igorkolesnik@MacBook-Pro--Ihor.local>
2023-03-15 15:59:56 +0100

    minor update

--- src/App/components/Modal/Modal.jsx
+++ src/App/components/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import MuiDialog from "@mui/material/Dialog";
 import MuiDialogContent from "@mui/material/DialogContent";
 
 export const Modal = ({ open = false, children, onClose }) => (
-  <MuiDialog open={open} maxWidth="lg" fullWidth onClose={onClose}>
+  <MuiDialog open={open} maxWidth="md" fullWidth onClose={onClose}>
     <MuiDialogContent>{children}</MuiDialogContent>
   </MuiDialog>
 );
